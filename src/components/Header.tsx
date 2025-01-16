27347a3b9ce53a2bbcc9fd6c6ef9669c8b14ee30
Bimal <bimal_shah2@hotmail.com>
2025-01-16 16:31:55 +0000

    added s

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 const Header = () => {
   return (
     <div className='w-full bg-lime-800 flex justify-between'>
-        <div className='text-white text-4xl p-2 mx-auto'>Todo App</div>
+        <div className='text-white text-4xl p-2 mx-auto'>Todo Apps</div>
         <div className='text-white text-xl p-2 ml-auto'>
         <a
           href="https://github.com/bimalshah2"
